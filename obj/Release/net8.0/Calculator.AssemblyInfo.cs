c8fbb08603a6cdef5a56594517d7fd94dca1e55d
emilcobarrubia <150938097+emilcobarrubia@users.noreply.github.com>
2024-06-17 21:39:56 -0400

    Cleared WeatherForecast example from Calculator solution

--- obj/Release/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Release/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+173b4826274d47dc406c8b5e0721c48d0893e4a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c040c446986a4991619dec5799f89dbf9f0f43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
