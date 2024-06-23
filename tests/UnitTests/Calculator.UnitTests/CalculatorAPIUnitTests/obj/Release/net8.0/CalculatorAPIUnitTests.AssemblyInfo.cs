adac9d8495768789279b599f8fafa16ab39e70fe
Emil Cobarrubia <Emil.Cobarrubia@gmail.com>
2024-06-22 23:02:05 -0400

    Updated gitignore

--- tests/UnitTests/Calculator.UnitTests/CalculatorAPIUnitTests/obj/Release/net8.0/CalculatorAPIUnitTests.AssemblyInfo.cs
+++ tests/UnitTests/Calculator.UnitTests/CalculatorAPIUnitTests/obj/Release/net8.0/CalculatorAPIUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorAPIUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb7f76fd4aaa23060f9051d7aae5b13dd504a94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e79e07e432d7950ab1eb878247b8b691e2af10")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorAPIUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorAPIUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
