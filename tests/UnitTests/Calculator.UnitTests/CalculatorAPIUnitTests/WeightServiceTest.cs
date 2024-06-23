899daba4a7c0c2843bcc4f9d04d70a640e01b460
Emil Cobarrubia <Emil.Cobarrubia@gmail.com>
2024-06-23 12:01:31 -0400

    Added UnitTest for Moon Weight

---
+++ tests/UnitTests/Calculator.UnitTests/CalculatorAPIUnitTests/WeightServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Calculator.DTOs;
+using Calculator.Services;
+using Weight.Services;
+namespace CalculatorAPIUnitTests;
+
+public class WeightServiceTest
+{
+    [Fact]
+    public void Weight_On_Moon_Is_OneSixth_Of_Earth()
+    {
+        //Arrange
+        WeightService testCase = new WeightService();
+        double expected = 0.1666;
+        
+        //Act
+        WeightResponseDto response = testCase.GetWeightOnMoon(1, null);
+
+        //Assert
+        Assert.Equal(Math.Round(expected, 2), Math.Round(response.Weight, 2));
+    }
+}
