a1385a410fbb3d159d48242bf79a1eca869591bc
Emil Cobarrubia <Emil.Cobarrubia@gmail.com>
2024-06-23 11:32:19 -0400

    Added bin and obj folders to gitignore.

--- tests/UnitTests/Calculator.UnitTests/CalculatorAPIUnitTests/obj/Debug/net8.0/CalculatorAPIUnitTests.AssemblyInfo.cs
+++ tests/UnitTests/Calculator.UnitTests/CalculatorAPIUnitTests/obj/Debug/net8.0/CalculatorAPIUnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorAPIUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c683cac31c956cd3112aa5e1524e854c9de6452e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c47e9652b4130b5e05893d255234499ff81c572")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorAPIUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorAPIUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
