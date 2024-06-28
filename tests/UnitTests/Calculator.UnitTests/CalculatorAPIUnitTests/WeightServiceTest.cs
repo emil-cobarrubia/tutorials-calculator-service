d1a912a2deeb5ed2a0574d6dba6ed11b11824a0b
Emil Cobarrubia <emil.cobarrubia@gmail.com>
2024-06-27 20:44:31 -0400

    Implemented Weight on Mercury unit test.

--- tests/UnitTests/Calculator.UnitTests/CalculatorAPIUnitTests/WeightServiceTest.cs
+++ tests/UnitTests/Calculator.UnitTests/CalculatorAPIUnitTests/WeightServiceTest.cs
@@ -6,7 +6,7 @@ namespace CalculatorAPIUnitTests;
 public class WeightServiceTest
 {
     [Fact]
-    public void Weight_On_Moon_Is_OneSixth_Of_Earth()
+    public void Weight_On_Moon_Factor()
     {
         //Arrange
         WeightService testCase = new WeightService();
@@ -15,6 +15,20 @@ public class WeightServiceTest
         //Act
         WeightResponseDto response = testCase.GetWeightOnMoon(1, null);
 
+        //Assert
+        Assert.Equal(Math.Round(expected, 2), Math.Round(response.Weight, 2));
+    }
+
+    [Fact]
+    public void Weight_On_Mercury_Factor()
+    {
+        //Arrange
+        WeightService testCase = new WeightService();
+        double expected = 0.3773;
+
+        //Act
+        WeightResponseDto response = testCase.GetWeightOnMercury(1, null);
+        
         //Assert
         Assert.Equal(Math.Round(expected, 2), Math.Round(response.Weight, 2));
     }
