2071583973bcccaa7cb472cd7624efc4838d25a7
Emil Cobarrubia <emil.cobarrubia@gmail.com>
2024-06-28 09:43:47 -0400

    Added additional unit tests for other planets.

--- tests/UnitTests/Calculator.UnitTests/CalculatorAPIUnitTests/WeightServiceTest.cs
+++ tests/UnitTests/Calculator.UnitTests/CalculatorAPIUnitTests/WeightServiceTest.cs
@@ -34,6 +34,104 @@ public class WeightServiceTest
     }
 
     [Fact]
+    public void Weight_On_Venus_Factor()
+    {
+        //Arrange
+        WeightService testCase = new WeightService();
+        double expected = 0.9045;
+
+        //Act
+        ServiceResponse<WeightResponseDto> response = testCase.GetWeightOnVenus(1, null);
+        
+        //Assert
+        Assert.Equal(Math.Round(expected, 2), Math.Round(response.Data.Weight, 2));
+    }
+
+    [Fact]
+    public void Weight_On_Mars_Factor()
+    {
+        //Arrange
+        WeightService testCase = new WeightService();
+        double expected = 0.3794;
+
+        //Act
+        ServiceResponse<WeightResponseDto> response = testCase.GetWeightOnMars(1, null);
+        
+        //Assert
+        Assert.Equal(Math.Round(expected, 2), Math.Round(response.Data.Weight, 2));
+    }
+
+    [Fact]
+    public void Weight_On_Jupiter_Factor()
+    {
+        //Arrange
+        WeightService testCase = new WeightService();
+        double expected = 2.5279;
+
+        //Act
+        ServiceResponse<WeightResponseDto> response = testCase.GetWeightOnJupiter(1, null);
+        
+        //Assert
+        Assert.Equal(Math.Round(expected, 2), Math.Round(response.Data.Weight, 2));
+    }
+
+    [Fact]
+    public void Weight_On_Saturn_Factor()
+    {
+        //Arrange
+        WeightService testCase = new WeightService();
+        double expected = 1.0646;
+
+        //Act
+        ServiceResponse<WeightResponseDto> response = testCase.GetWeightOnSaturn(1, null);
+        
+        //Assert
+        Assert.Equal(Math.Round(expected, 2), Math.Round(response.Data.Weight, 2));
+    }
+
+    [Fact]
+    public void Weight_On_Uranus_Factor()
+    {
+        //Arrange
+        WeightService testCase = new WeightService();
+        double expected = 0.8861;
+
+        //Act
+        ServiceResponse<WeightResponseDto> response = testCase.GetWeightOnUranus(1, null);
+        
+        //Assert
+        Assert.Equal(Math.Round(expected, 2), Math.Round(response.Data.Weight, 2));
+    }
+
+    [Fact]
+    public void Weight_On_Neptune_Factor()
+    {
+        //Arrange
+        WeightService testCase = new WeightService();
+        double expected = 1.1367;
+
+        //Act
+        ServiceResponse<WeightResponseDto> response = testCase.GetWeightOnNeptune(1, null);
+        
+        //Assert
+        Assert.Equal(Math.Round(expected, 2), Math.Round(response.Data.Weight, 2));
+    }
+
+    [Fact]
+    public void Weight_On_Pluto_Factor()
+    {
+        //Arrange
+        WeightService testCase = new WeightService();
+        double expected = 0.0632;
+
+        //Act
+        ServiceResponse<WeightResponseDto> response = testCase.GetWeightOnPluto(1, null);
+        
+        //Assert
+        Assert.Equal(Math.Round(expected, 2), Math.Round(response.Data.Weight, 2));
+    }
+
+    [Fact]
     public void Weight_Negative_Error()
     {
         //Arrange
